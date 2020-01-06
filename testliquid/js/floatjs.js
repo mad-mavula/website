5b3b2d23477adbc14f6752d99a655cca0e0518f7
mad-mavula <59197160+mad-mavula@users.noreply.github.com>
2020-01-06 19:33:09 +0530

    updated

--- testliquid/js/floatjs.js
+++ testliquid/js/floatjs.js
@@ -1,14 +1,10 @@
-document.getElementsByClassName('login').addEventListener('click', function(){
+document.getElementsById('login').addEventListener('click', function(){
         var check=<?php echo json_encode($logged) ?>
-        if (check==1){document.querySelector('.float-bg').style.display='flex';}
+if(check==1 ){document.querySelector('.float-bg').style.display='flex';}
 	
 });
 
 document.querySelector('.exit').addEventListener('click', function(){
 	document.querySelector('.float-bg').style.display='none';
 });
-
-function checklogin(){
-
-
 
